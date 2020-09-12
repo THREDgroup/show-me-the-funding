858bf51bade6b89e4e2a483eb2c1d4fd11df2826
Chris McComb <ccmcc2012@gmail.com>
2020-09-12 17:36:20 -0400

    Better program search too

--- javascript/main.js
+++ javascript/main.js
@@ -16,6 +16,13 @@ let offset = 1;
 let CORS_anywhere = "https://cors-anywhere.herokuapp.com/";
 let spinner = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>  ';
 
+// Plotly
+let configuration = {
+    responsive: true,
+    scrollZoom: false,
+    displayModeBar: true
+};
+
 $(function () {
     let min_handle = $( "#custom-handle-min" );
     let max_handle = $( "#custom-handle-max" );
@@ -43,6 +50,7 @@ function prepare_for_load() {
     $("#results_description").html("");
     $("#money_histogram").html("");
     $("#time_histogram").html("");
+    $("#money_time_histogram").html("");
     $("#slider-range").hide()
 
     // Reset hte data
@@ -94,10 +102,19 @@ function initial_parse(program_string) {
     let min_time = Math.min(...years);
     let max_time = Math.max(...years);
 
-    update_slider(min_time, max_time);
+    // Activate tab adn make summary
     make_summary(min_time, max_time, program_string);
-    make_funding_histogram(min_time, max_time);
+    $("#summary-tab").removeClass("disabled");
+
+    // Another tab
     make_time_histogram(years);
+    make_money_time_histogram(years);
+    $("#program-size-tab").removeClass("disabled");
+
+    // Another tab
+    update_slider(min_time, max_time);
+    make_funding_histogram(min_time, max_time);
+    $("#award-size-tab").removeClass("disabled");
 
 }
 
@@ -118,7 +135,6 @@ function make_time_histogram(years) {
 
     let layout = {yaxis: {title: {text: "Count"}}, xaxis: {title: {text: "Year"}, tick0: Math.min(...years), dtick: 1.0}};
 
-    let configuration = {responsive: true};
     Plotly.newPlot('time_histogram', data2, layout, configuration);
 
 }
@@ -134,6 +150,7 @@ function make_summary(min_time, max_time, program_string) {
 
     $("#results_description").html("<p>Between the years of " + min_time + " and " + max_time + ", the <b>" + program_string + "</b> program funded " + n + " projects.</p> <p>For these projects, there was a median funding level of $" + p50 + ", with 25th and 75th percentiles of $" + p25 + " and $" + p75 + ", respectively.")
 }
+
 function get_money(min_time, max_time) {
     return all_data.filter(function(e) {
         return e.startYear >= min_time && e.startYear <= max_time;
@@ -154,12 +171,27 @@ function make_funding_histogram(min_time, max_time) {
 
     let layout = {yaxis: {title: {text: "Number of Awards"}}, xaxis: {title: {text: "Funding"}}};
 
-    let configuration = {responsive: true};
-
     Plotly.newPlot('money_histogram', data, layout, configuration);
 }
 
 
+function make_money_time_histogram(years) {
+    let money = get_money(Math.min(...years), Math.max(...years))
+
+    let trace2 = {
+        histfunc: "sum",
+        x: years,
+        y: money,
+        type: 'histogram',
+        xbins: {size: 1},
+    };
+    let data2 = [trace2];
+
+    let layout = {yaxis: {title: {text: "Total Funding Obligated"}}, xaxis: {title: {text: "Year"}, tick0: Math.min(...years), dtick: 1.0}};
+
+    Plotly.newPlot('money_time_histogram', data2, layout, configuration);
+
+}
 
 
 
